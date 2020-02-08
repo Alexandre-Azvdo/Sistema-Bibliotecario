26e5e2e8767de12a780668f80c985fa1bb3c4ec1
Alexandre-Azvdo <azvdo.ads@gmail.com>
2020-02-08 08:57:41 -0300

    Endpoint /funcionarios/{id}

---
+++ src/main/java/com/alexandre/biblioteca/services/FuncionarioService.java
@@ -0,0 +1,23 @@
+package com.alexandre.biblioteca.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandre.biblioteca.domain.Funcionario;
+import com.alexandre.biblioteca.repositories.FuncionarioRepository;
+import com.alexandre.biblioteca.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class FuncionarioService {
+
+	@Autowired
+	private FuncionarioRepository repo;
+	
+	public Funcionario buscar(Integer id) {
+		Optional<Funcionario> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Funcionario.class.getName()));
+	}
+}
